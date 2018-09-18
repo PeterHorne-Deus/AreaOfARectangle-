48a89c152f560ecf414bfcd7951b00e08d1cffbf
pehor9164 <pehor9164@7KWN6Y1.Student.UGDSB.ED>
2018-09-18 09:26:37 -0400

    modified

--- src/areaofarectangle/AreaOfARectangle.java
+++ src/areaofarectangle/AreaOfARectangle.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Peter Horne-Deus
+ * AreaOfARectangle.java
+ * This program calculates the area of a rectangle with the length of 5.7m and 4.8m
+ * September 18, 2018
  */
 
 package areaofarectangle;
